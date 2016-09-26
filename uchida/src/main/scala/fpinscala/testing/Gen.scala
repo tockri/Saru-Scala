c579aa53a1c5806a6e1f041cef60258ab6238a56
uchidayuichi <uchida@nulab-inc.com>
2016-09-26 19:28:03 +0900

    Add.

--- uchida/src/main/scala/fpinscala/testing/Gen.scala
+++ uchida/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,9 +1,28 @@
 package fpinscala.testing
 
+import fpinscala.state.RNG.Rand
 import fpinscala.state.{RNG, State}
 
 case class Gen[A](sample: State[RNG, A])
 
 object Gen {
-  def choose(start: Int, stopExclusive: Int): Gen[Int] = ???
+
+//  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+//    RNG.Simple(1)
+//    val state: State[RNG, Int] = State.unit(1)
+//    Gen(state)
+//  }
+
+  def choose(start: Int, stopExclusive: Int): Gen[Int] =
+    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive-start)))
+
+  def unit[A](a: => A): Gen[A] =
+    Gen(State.unit(a))
+
+  def boolean: Gen[Boolean] =
+    Gen(State(RNG.boolean))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State.sequence(List.fill(n)(g.sample)))
+
 }
