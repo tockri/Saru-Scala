29f63a4667656fa51b22606d68ee4ae670d3904e
Tama (Masanori Fujta) <fujita@nulab-inc.com>
2016-10-17 20:06:09 +0900

    listOfNを使う

--- fujita/src/main/scala/property/Study.scala
+++ fujita/src/main/scala/property/Study.scala
@@ -12,7 +12,7 @@ object Study {
     //val c = (new TestProp && new TestProp).check
     //println(c)
     val intGen:Gen[Int] = Gen(State {rng =>rng.nextInt})
-    val gen:Gen[List[Int]] = Gen.listOfN(10, intGen)
+    val gen:Gen[List[Int]]  = intGen.listOfN(10, intGen)
     println("ランダムな配列")
     println(gen.sample.run(RNG.Simple(10000L))._1)
 
@@ -38,8 +38,12 @@ class TestProp extends Prop {
   * @tparam A
   */
 case class Gen[A](sample:State[RNG, A]) {
+
   def flatMap[B](f: A => Gen[B]): Gen[B] =
     Gen(sample.flatMap(a => f(a).sample))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State.sequence(List.fill(n)(g.sample)))
 
 }
 
@@ -48,10 +52,6 @@ object Gen {
   def unit[A](a: => A):Gen[A] = Gen(State(RNG.unit(a)))
 
   def boolean: Gen[Boolean] = Gen(State(RNG.boolean))
-
-  def listOfN[A](n: Int, g: Gen[A]):Gen[List[A]] =
-    Gen(State.sequence(List.fill(n)(g.sample)))
-
 
   def choose(start:Int, stopExclusive:Int):Gen[Int] = {
     val state:State[RNG,Int] = State(RNG.nonNegativeInt)
