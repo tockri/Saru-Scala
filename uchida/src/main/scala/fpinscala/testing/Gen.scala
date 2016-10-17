5e798289c7e8147a10111a83bfcd588695140d68
uchidayuichi <uchida@nulab-inc.com>
2016-10-17 19:56:03 +0900

    Add.

--- uchida/src/main/scala/fpinscala/testing/Gen.scala
+++ uchida/src/main/scala/fpinscala/testing/Gen.scala
@@ -1,20 +1,27 @@
 package fpinscala.testing
 
-import fpinscala.state.RNG.Rand
 import fpinscala.state.{RNG, State}
 
-case class Gen[A](sample: State[RNG, A])
+case class Gen[A](sample: State[RNG, A]) {
+
+  def flatMap[B](f: A => Gen[B]): Gen[B] =
+    Gen(sample.flatMap(a => f(a).sample))
+
+  def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
+    Gen(State.sequence(List.fill(n)(g.sample)))
+
+}
 
 object Gen {
 
-//  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
-//    RNG.Simple(1)
-//    val state: State[RNG, Int] = State.unit(1)
-//    Gen(state)
-//  }
+  //  def choose(start: Int, stopExclusive: Int): Gen[Int] = {
+  //    RNG.Simple(1)
+  //    val state: State[RNG, Int] = State.unit(1)
+  //    Gen(state)
+  //  }
 
   def choose(start: Int, stopExclusive: Int): Gen[Int] =
-    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive-start)))
+    Gen(State(RNG.nonNegativeInt).map(n => start + n % (stopExclusive - start)))
 
   def unit[A](a: => A): Gen[A] =
     Gen(State.unit(a))
@@ -24,5 +31,16 @@ object Gen {
 
   def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
     Gen(State.sequence(List.fill(n)(g.sample)))
+
+}
+
+object Test {
+
+  def main(args: Array[String]): Unit = {
+
+    val gen: Gen[List[Int]] = Gen.listOfN(10, Gen.unit(2))
+    println("ランダムな配列")
+    println(gen.sample.run(RNG.Simple(10000L))._1)
+  }
 
 }
