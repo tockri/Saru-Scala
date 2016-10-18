e5cb501d884ffbcfe8699551451727e2aeb93316
uchidayuichi <uchida@nulab-inc.com>
2016-10-18 09:34:32 +0900

    Add exersice 8.7.

--- uchida/src/main/scala/fpinscala/testing/Gen.scala
+++ uchida/src/main/scala/fpinscala/testing/Gen.scala
@@ -32,6 +32,9 @@ object Gen {
   def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] =
     Gen(State.sequence(List.fill(n)(g.sample)))
 
+  def union[A](g1: Gen[A], g2: Gen[A]): Gen[A] =
+    boolean.flatMap(b => if (b) g1 else g2)
+
 }
 
 object Test {
