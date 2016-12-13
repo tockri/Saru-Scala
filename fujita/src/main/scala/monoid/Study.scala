cc73f99316ebbf08390f91971e0c1e58365994bd
Tama (Masanori Fujta) <fujita@nulab-inc.com>
2016-12-13 10:07:01 +0900

    12/12までの内容

--- fujita/src/main/scala/monoid/Study.scala
+++ fujita/src/main/scala/monoid/Study.scala
@@ -220,10 +220,24 @@ object Study {
       def zero = _ => B.zero
     }
 
-  def bag[A](as: IndexedSeq[A]): Map[A, Int] = as.foldLeft(Map[A, Int]())(acc, a => )
+  def mapMergeMonoid[K,V](V: Monoid[V]): Monoid[Map[K, V]] =
+    new Monoid[Map[K, V]] {
+      def zero = Map[K,V]()
+      def op(a: Map[K, V], b: Map[K, V]) =
+        (a.keySet ++ b.keySet).foldLeft(zero) { (acc,k) =>
+          acc.updated(k, V.op(a.getOrElse(k, V.zero),
+            b.getOrElse(k, V.zero)))
+        }
+    }
+
+  def bag[A](as: IndexedSeq[A]): Map[A, Int] =
+    foldMapV(as, mapMergeMonoid[A, Int](intAddition))((a: A) => Map(a -> 1))
+//    as.foldLeft(Map[A, Int]())((acc:Map[A, Int], key:A) => acc.updated(key, acc.getOrElse(key, 0) + 1))
 
   def main(args: Array[String]): Unit = {
     println("Saru")
+
+    println(bag(Vector("a", "rose", "is", "a", "rose")))
 //    val a1: Int => Int = i => 2 * i
 //    val a2: Int => Int = i => 3 * i
 //    val c = endoMonoid.op(a1, a2)
